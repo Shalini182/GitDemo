773e4b5c13d6b704100f84e1dcbb6e3de0497813
shalini <shalini.24@capgemini.com>
2024-05-15 21:25:19 +0530

    first commit

---
+++ src/Method.java
@@ -0,0 +1,12 @@
+
+public class Method {
+	
+	//public static void main(String[] args)
+	//public-access modifier,  void-return type
+	public void ValidateHeader()
+	{
+		System.out.println("validate header link");
+		//return "Pass";
+	}
+
+}
---
+++ src/Parent.java
@@ -0,0 +1,32 @@
+
+public class Parent {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Started java programming");
+		/*
+		//add 2 no.2.3.-5
+		
+		int a=2,b=3,sum;
+		//int b=3;
+		sum=a+b;
+		System.out.println(sum);
+		System.out.println("Sum is " + sum);*/
+		
+		System.out.println("I navigated to home page");
+		
+		Method m = new Method();
+		//String a= m.ValidateHeader();
+		//System.out.println(m.ValidateHeader());
+		m.ValidateHeader();
+				
+		
+		
+	}
+	
+	//10 lines of code for header display -constant header displaying
+	
+	
+
+}
+
